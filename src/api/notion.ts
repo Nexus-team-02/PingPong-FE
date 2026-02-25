7277bdec3e4e3cce997b21800d354131e3c0edc6
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-02-26 02:41:34 +0900

    API/#51/노션 페이지 연동 API

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,5 +1,6 @@
 import { client } from './client'
 import { handleApiError } from './handleApiError'
+import { CreatePageRequest, UpdatePageRequest } from '@/types/notion'
 
 export async function exchangeNotionToken(teamId: number, code: string) {
   try {
@@ -41,9 +42,46 @@ export async function selectDatabase(teamId: number, databaseId: string) {
   }
 }
 
-export async function getTasks(teamId: number) {
+export async function getPrimaryDatabase(teamId: number) {
   try {
     const res = await client.get(`/api/v1/teams/${teamId}/notion/databases/primary`)
+    console.log(res)
+    return res.data.result
+  } catch (error) {
+    throw handleApiError(error)
+  }
+}
+
+export async function createPrimaryPage(teamId: number, body: CreatePageRequest) {
+  try {
+    const res = await client.post(`/api/v1/teams/${teamId}/notion/databases/primary`, body)
+    return res.data.result
+  } catch (error) {
+    throw handleApiError(error)
+  }
+}
+
+export async function getPageDetail(teamId: number, pageId: string) {
+  try {
+    const res = await client.get(`/api/v1/teams/${teamId}/notion/pages/${pageId}`)
+    return res.data.result
+  } catch (error) {
+    throw handleApiError(error)
+  }
+}
+
+export async function createChildDatabase(teamId: number, pageId: string) {
+  try {
+    const res = await client.post(`/api/v1/teams/${teamId}/notion/pages/${pageId}/databases`)
+    return res.data.result
+  } catch (error) {
+    throw handleApiError(error)
+  }
+}
+
+export async function updatePage(teamId: number, pageId: string, body: UpdatePageRequest) {
+  try {
+    const res = await client.patch(`/api/v1/teams/${teamId}/notion/pages/${pageId}`, body)
     return res.data.result
   } catch (error) {
     throw handleApiError(error)
